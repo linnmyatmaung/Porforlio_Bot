ce06d309ed6da2a18f4c6a8bec3dcccd4562a4a1
linnmyatmaung <linnmyatmaung03@gmail.com>
2025-05-08 15:58:59 +0630

    Porforlio Chat-Bot

--- src/app/components/ChatInitiation.tsx
+++ src/app/components/ChatInitiation.tsx
@@ -8,15 +8,11 @@ const ChatInitiation = () => {
         <BotAvatar width="w-15" height="h-15" />
       </div>
       <h2 className="text-lg md:text-2xl font-semibold">
-        {getGreetingMessage()} My Brothers | Sisters
+        {getGreetingMessage()}
       </h2>
       <h2 className="text-lg md:text-2xl font-semibold">
         Ask me anything you want to know about me!
       </h2>
-      <p className="text-slate-400 text-sm mt-2">
-        Choose a prompt below or write your own to start chatting with Porforlio
-        Bot!
-      </p>
     </div>
   );
 };
